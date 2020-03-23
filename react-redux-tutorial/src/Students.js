c85d898f823ead64775cdbc17cc42c4f46f55ffd
milomacphail <milomacphail@gmail.com>
2020-03-23 15:32:03 -0400

    First component

---
+++ react-redux-tutorial/src/Students.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Students extends Component {
+  render() {
+    return (
+      <div className="student">
+        <div>Name: Milo</div>
+        <div>Age: 31</div>
+        <div>Role: Student</div>
+      </div>
+    );
+  }
+}
+export default Students;
